3a70ea9d1ce2e7f90f9bcb1d69bf98c6326c0008
Jacky Bourgeois <j.bourgeois@tudelft.nl>
2019-07-04 17:41:34 +0200

    debug

--- index.js
+++ index.js
@@ -114,82 +114,81 @@ function processValue(values, propertyId) {
 
 function checkActivityAndCount() {
   logger.debug('check activity and count');
-  // for (let key in thingMap) {
-  //   if (!thingMap.hasOwnProperty(key)) continue;
-  //   let thing = thingMap[key];
-  //   if (thing.currentPeriodDataCount > 0) {
-  //     // assume that we will receive a values for the count, so we start at -1
-  //     thing.currentPeriodDataCount = -1;
-  //     // update the count value
-  //     model.properties.updateValues(new Property({
-  //       id: thing.dataCountId,
-  //       values: [[Date.now(), thing.currentPeriodDataCount]]
-  //     }));
-  //     // No new data point, check if we should switch the activity state to busy (1)
-  //     if (thing.activity === 0) {
-  //       // it was quiet, it became busy!
-  //       // change the activity state
-  //       thing.activity = 1;
-  //       // decrease the data counter to escape the activity data point
-  //       thing.currentPeriodDataCount--;
-  //       // update the activity value
-  //       model.properties.updateValues(new Property({
-  //         id: thing.dataCountId,
-  //         values: [[Date.now(), thing.activity]]
-  //       }));
-  //     }
-  //   }
-  //   // No new data point, check if we should switch the activity state to quiet (0)
-  //   else if (thing.activity === 1) {
-  //     // it was busy, it became quiet!
-  //     // change the activity state
-  //     thing.activity = 1;
-  //     // decrease the data counter to escape the activity data point
-  //     thing.currentPeriodDataCount--;
-  //     // update the activity value
-  //     model.properties.updateValues(new Property({
-  //       id: thing.dataActivityId,
-  //       values: [[Date.now(), thing.activity]]
-  //     }));
-  //   }
-  // }
+  for (let key in thingMap) {
+    if (!thingMap.hasOwnProperty(key)) continue;
+    let thing = thingMap[key];
+    if (thing.currentPeriodDataCount > 0) {
+      // assume that we will receive a values for the count, so we start at -1
+      thing.currentPeriodDataCount = -1;
+      // update the count value
+      model.properties.updateValues(new Property({
+        id: thing.dataCountId,
+        values: [[Date.now(), thing.currentPeriodDataCount]]
+      }));
+      // No new data point, check if we should switch the activity state to busy (1)
+      if (thing.activity === 0) {
+        // it was quiet, it became busy!
+        // change the activity state
+        thing.activity = 1;
+        // decrease the data counter to escape the activity data point
+        thing.currentPeriodDataCount--;
+        // update the activity value
+        model.properties.updateValues(new Property({
+          id: thing.dataCountId,
+          values: [[Date.now(), thing.activity]]
+        }));
+      }
+    }
+    // No new data point, check if we should switch the activity state to quiet (0)
+    else if (thing.activity === 1) {
+      // it was busy, it became quiet!
+      // change the activity state
+      thing.activity = 1;
+      // decrease the data counter to escape the activity data point
+      thing.currentPeriodDataCount--;
+      // update the activity value
+      model.properties.updateValues(new Property({
+        id: thing.dataActivityId,
+        values: [[Date.now(), thing.activity]]
+      }));
+    }
+  }
 }
 
 function createThingProperties(entityId) {
   logger.debug('create thing prop ' + entityId);
-  return Promise.resolve();
   // let dataCountPropId, dataActivityPropId;
-  // model.properties
-  //   // create a property for the data count
-  //   .create(new Property({
-  //     name: 'Data Count',
-  //     type: 'COUNT',
-  //     entityId: entityId
-  //   }))
-  //   // create a property for the data activity
-  //   .then(property => {
-  //     dataCountPropId = property.id;
-  //     return model.properties
-  //       .create(new Property({
-  //         name: 'Data Activity',
-  //         class: ['Quiet', 'Busy'],
-  //         type: 'CLASS',
-  //         entityId: entityId
-  //       }))
-  //   })
-  //   // add the thing to the thing map
-  //   .then((property) => {
-  //     dataActivityPropId = property.id;
-  //     thingMap[entityId] = {
-  //       id: entityId,
-  //       currentPeriodDataCount: 0,
-  //       activity: 0,
-  //       dataCountId: dataCountPropId,
-  //       dataActivityId: dataActivityPropId
-  //     };
-  //     return Promise.resolve();
-  //   })
-  //   .catch(error => logger.error(error));
+  model.properties
+    // create a property for the data count
+    .create(new Property({
+      name: 'Data Count',
+      type: 'COUNT',
+      entityId: entityId
+    }))
+    // create a property for the data activity
+    .then(property => {
+      dataCountPropId = property.id;
+      return model.properties
+        .create(new Property({
+          name: 'Data Activity',
+          class: ['Quiet', 'Busy'],
+          type: 'CLASS',
+          entityId: entityId
+        }))
+    })
+    // add the thing to the thing map
+    .then((property) => {
+      dataActivityPropId = property.id;
+      thingMap[entityId] = {
+        id: entityId,
+        currentPeriodDataCount: 0,
+        activity: 0,
+        dataCountId: dataCountPropId,
+        dataActivityId: dataActivityPropId
+      };
+      return Promise.resolve();
+    })
+    .catch(error => logger.error(error));
 }
 
 setInterval(checkActivityAndCount, process.env.CHECK_ACTIVITY_COUNT || 60000);
